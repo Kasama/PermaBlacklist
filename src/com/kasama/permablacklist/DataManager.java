03468e92782bef30b37c95ead5e164f316b2df42
Kasama <ralegro@bol.com.br>
2015-07-06 02:20:35 -0300

    added cleanup code to database

--- src/com/kasama/permablacklist/DataManager.java
+++ src/com/kasama/permablacklist/DataManager.java
@@ -3,19 +3,21 @@ package com.kasama.permablacklist;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataManager {
 
+	Connection conn;
+	Statement statement;
+
 	public DataManager(String database) {
 		File file = new File(database);
 		boolean dbExists = file.exists();
-		Connection c;
-		Statement statement;
 		try {
 			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
-			statement = c.createStatement();
+			conn = DriverManager.getConnection("jdbc:sqlite:" + file.getAbsolutePath());
+			statement = conn.createStatement();
 			String sql;
 			if (!dbExists) {
 				sql = "CREATE TABLE blacklist" +
@@ -28,10 +30,27 @@ public class DataManager {
 //			sql = "INSERT INTO BLACKLISTEDS (ID, NAME, CPFCNPJ)" +
 //				"VALUES (1, 'Jose', '12786421');";
 //			statement.execute(sql);
-			statement.close();
-			c.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+		} catch (SQLException e) {
+			System.err.println("An exception was thrown while trying to open the database connection!");
+		} catch (ClassNotFoundException e) {
+			System.err.println("Fatal error: Could not find class JDBC!");
+		} finally {
+			cleanUp();
 		}
+	}
+
+	public void cleanUp(){
+		try {
+			statement.close();
+			conn.close();
+		} catch (SQLException e) {
+			System.err.println("An exception was thrown while trying to close the connection to the database");
+		}
+	}
+
+	@Override
+	public void finalize() throws Throwable {
+		cleanUp();
+		super.finalize();
 	}
 }
